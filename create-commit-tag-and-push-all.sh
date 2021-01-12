de8f19666d799ec61297876a825771b12fc64cda
alejandro <alejandro.amendola@gmail.com>
2021-01-11 22:56:26 -0300

    Automatic commit message

--- create-commit-tag-and-push-all.sh
+++ create-commit-tag-and-push-all.sh
@@ -14,4 +14,4 @@ COMMIT_HASH=`git log --pretty=format:'%h' -n 1`
 git tag -a $VERSION -m "Last commit $COMMIT_HASH"
 git push origin $BRANCH
 git push origin --tags
-git tag | tail -1
+echo version:$VERSION
